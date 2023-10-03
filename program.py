ceee8a69db89f38a86c8344a42bdc59d476cc2e2
kieranHH <144241789+kieranHH@users.noreply.github.com>
2023-10-03 08:53:16 -0400

    Update program.py

--- program.py
+++ program.py
@@ -1,6 +1,6 @@
 # My Python Program
 # Task:  Use the function myFunction to output a simple "Hello World!" statement
 
-def myFunction():
-  print("hello, world")
+def my_Function():
+  print("Hello World!")
   # My Function
