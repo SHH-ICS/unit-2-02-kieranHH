aa2142e5a111de9672466375b400e24d656115dd
kieranHH <144241789+kieranHH@users.noreply.github.com>
2023-10-03 08:46:04 -0400

    Update program.py

--- program.py
+++ program.py
@@ -1,5 +1,11 @@
 # My Python Program
 # Task:  Use the function myFunction to output a simple "Hello World!" statement
-
+<h3>First webpage</h3>
+<p>Hello World</p>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Unit2Lesson2</title>
+</head>
 def myFunction():
   # My Function
